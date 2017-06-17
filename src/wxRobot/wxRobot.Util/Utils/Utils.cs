85b272095c5fd5633a2e4048fe14139f423ed1ef
刘发源 <fayuanliu@outlook.com>
2017-06-17 18:36:19 +0800

    增加获取目录权限

--- src/wxRobot/wxRobot.Util/Utils/Utils.cs
+++ src/wxRobot/wxRobot.Util/Utils/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 
 namespace wxRobot.Util.Utils
@@ -44,6 +45,47 @@ namespace wxRobot.Util.Utils
         {
             TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        }
+
+        /// <summary>
+        /// 为指定用户组，授权目录指定完全访问权限
+        /// </summary>
+        /// <param name="user">用户组，如Users</param>
+        /// <param name="folder">实际的目录</param>
+        /// <returns></returns>
+        private static bool SetAccess(string user, string folder)
+        {
+            //定义为完全控制的权限
+            const FileSystemRights Rights = FileSystemRights.FullControl;
+
+            //添加访问规则到实际目录
+            var AccessRule = new FileSystemAccessRule(user, Rights,
+                InheritanceFlags.None,
+                PropagationFlags.NoPropagateInherit,
+                AccessControlType.Allow);
+
+            var Info = new DirectoryInfo(folder);
+            var Security = Info.GetAccessControl(AccessControlSections.Access);
+
+            bool Result;
+            Security.ModifyAccessRule(AccessControlModification.Set, AccessRule, out Result);
+            if (!Result) return false;
+
+            //总是允许再目录上进行对象继承
+            const InheritanceFlags iFlags = InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit;
+
+            //为继承关系添加访问规则
+            AccessRule = new FileSystemAccessRule(user, Rights,
+                iFlags,
+                PropagationFlags.InheritOnly,
+                AccessControlType.Allow);
+
+            Security.ModifyAccessRule(AccessControlModification.Add, AccessRule, out Result);
+            if (!Result) return false;
+
+            Info.SetAccessControl(Security);
+
+            return true;
         }
     }
 }
